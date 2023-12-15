2a962c083f02dc686568f4ef232f8e524801867d
madMontersourus <105312512+madMontersourus@users.noreply.github.com>
2023-12-15 18:42:41 +0000

    decryptor working

--- hill climbing attack/bifid.py
+++ hill climbing attack/bifid.py
@@ -23,36 +23,42 @@ def fitness(text,EngTet):
         if each in EngTet:
             fitness += (CTTet[each]-EngTet[each])**2/EngTet[each]
     return(fitness)
-def bifidDecrypt(text,square):
-    coords = []
-    for each in text:
-        coord = np.array(np.where(square==each)).tolist()
-        for i in range(0,2):
-            num = ("".join(str(coord[i])).replace("[","")).replace("]","")
-            if num != "":
-                coord[i] = int(num)
-        coords.append(coord)
-        coords = [x for x in coords if x != [[],[]]]
+def bifidDecrypt(text,square,period):
+    text = [text[i:i+period] for i in range(0, len(text), period)]
 
+    plaintext = ""
+    for each in text:
+        coords = []
+        for i in each:
+            coord = np.array(np.where(square==i))
+            coord = (coord.flatten()).tolist()
+            for j in range(0,2):
+                coords.append(coord[j])
+        temp = coords[:len(coords)//2]
+        temp2 = coords[len(coords)//2:]
+        coords = [temp,temp2]
+        for i in range(len(coords[0])):
+            plaintext += square[coords[0][i]][coords[1][i]]
+    return plaintext
+margin = 1
+period = 4
 # set the parent key to a Polybius square with an unmixed alphabet
 alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 parentKey = np.array([['A', 'B', 'C', 'D', 'E'], ['F', 'G', 'H', 'I', 'K'], ['L', 'M', 'N', 'O', 'P'], ['Q', 'R', 'S', 'T', 'U'], ['V', 'W', 'X', 'Y', 'Z']])
-bifidDecrypt(ciphertext,parentKey)
 # set the best fitness to the fitness of the unmodified ciphertext
 bestFitness = fitness(ciphertext,EngTet)
-print(bestFitness)
 # set the counter to 0
-count = 0
+count = 51
 # while the counter is less than 10,000
-while count > 10000:
+while count < 50:
     # copy the parent key into a child key
     childKey = parentKey
     # randomly choose one of these modifications to the child key:
-    rand1 = random.randint(0,5)
-    rand2 = random.randint(0,5)
-    rand3 = random.randint(0,5)
-    rand4 = random.randint(0,5)
-    choice = random.randint(0,6)
+    rand1 = random.randint(0,4)
+    rand2 = random.randint(0,4)
+    rand3 = random.randint(0,4)
+    rand4 = random.randint(0,4)
+    choice = random.randint(0,5)
         # swap two randomly selected elements
     if choice == 0:
         temp = childKey[rand1][rand2]
@@ -60,7 +66,7 @@ while count > 10000:
         childKey[rand3][rand4] = temp
         # swap two randomly selected rows
     elif choice == 1:
-        childKey[rand1][rand2] = childKey[rand2][rand1]
+        childKey[[rand1,rand2]]=childKey[[rand2,rand1]]
         # swap two randomly selected columns
     elif choice == 2:
         childKey[:,[rand1,rand2]] = childKey[:,[rand2,rand1]]
@@ -75,12 +81,23 @@ while count > 10000:
     elif choice == 5:
         childKey = np.flip(childKey,axis=1)
     # decipher the ciphertext with the child key to find a new plaintext
+    trial = bifidDecrypt(ciphertext,childKey)
     # calculate the new fitness of the new plaintext
+    trialFitness = fitness(trial,EngTet)
     # if (the new fitness exceeds the best fitness) or
     # ((the new fitness exceed the best fitness minus the margin) and
     # (we roll a 1 on a 20-sided die))
+    if trialFitness < bestFitness or (trialFitness-margin and random.randint == 20): 
         # copy the new fitness to the best fitness
+        bestFitness = trialFitness
         # copy the child key into the parent key
+        parentKey = childKey
         # set the counter to 0
+        count = 0
     # increment the counter
+    count += 1
+    print(count)
 # output the parent key
+plaintext = bifidDecrypt("GRQSIISSADCPQQFUKHRQLEBVDWWSCPEY",parentKey,period)
+print(plaintext)
+print(parentKey)
